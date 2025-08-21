a9e01a98263c48d074dd980b8d46a999f001eea5
Alba Diaz <alba.dilop@gmail.com>
2025-08-21 11:07:19 +0000

    navbar

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/img/logo.png";
 
 export const Navbar = () => {
 
@@ -6,12 +7,31 @@ export const Navbar = () => {
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					    <a class="navbar-brand" href="#">
+      <img src={logo} alt="Logo" width="40" height="34" class="d-inline-block align-text-top "/>
+      The Second Life Hub
+    </a>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+					<nav class="navbar bg-body-tertiary">
+						<div class="container-fluid">
+							<form class="d-flex" role="search">
+						
+							
+								<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+						
+								<button class="btn btn-outline-success" type="submit">Search</button>
+								
+							</form>
+							
+						</div>
+					</nav>
+				</div>
+				<div>
+
+				<button type="button" class="btn btn-primary me-4">Register or Log-In</button>
+				
+				<button type="button" class="btn btn-primary">Sell</button>
 				</div>
 			</div>
 		</nav>
